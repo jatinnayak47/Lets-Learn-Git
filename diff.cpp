ad59bf339b09f5ad9362fbc1e50514fa23117362
JATIN NAYAK <jatinnayak0947@gmail.com>
2023-04-19 02:18:42 +0530

    Program to find the difference b/w two numbers

---
+++ diff.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i,j;
+    int diff;
+    cout<<"enter two number :";
+    cin>>i>>j;
+    diff=i-j;
+    cout<<"Difference b/w "<<i <<" & "<<j<<" : "<<diff;
+    return 0;
+}
