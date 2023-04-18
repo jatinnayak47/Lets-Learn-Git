1cc0727beb1917c288ef1d5027eae398642c0dc0
JATIN NAYAK <jatinnayak0947@gmail.com>
2023-04-19 02:11:10 +0530

    Initial Comment

---
+++ sum.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    int sum;
+    cout<<"Enter two number";
+    cin>>a>>b;
+    sum = a+b;
+    cout<<"The Sum Of Two Number Is :"<<sum;
+    return 0;
+}
