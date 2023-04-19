d920bb489edcddba49e913d59984ba4cb438b117
JATIN NAYAK <jatinnayak0947@gmail.com>
2023-04-19 08:45:24 +0530

    Program to multiply of two numbers

---
+++ multiply.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b,result;
+    cout<<"Enter two numbers :";
+    cin>>a>>b;
+    result = a* b;
+    cout<<"Multiple of "<<a<<" & "<<b<<" is "<<result;
+
+    return 0;
+}
