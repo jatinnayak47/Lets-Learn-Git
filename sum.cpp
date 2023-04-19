d0b3e058e46bfe7f0472708fe9d6d18459194c8d
JATIN NAYAK <jatinnayak0947@gmail.com>
2023-04-19 09:20:47 +0530

    Program to add three numbers

--- sum.cpp
+++ sum.cpp
@@ -3,11 +3,11 @@ using namespace std;
 
 int main()
 {
-    int a,b;
+    int a,b,c;
     int sum;
-    cout<<"Enter two number";
-    cin>>a>>b;
-    sum = a+b;
+    cout<<"Enter three number";
+    cin>>a>>b>>c;
+    sum = a+b+c;
     cout<<"The Sum Of Two Number  :"<<sum;
     return 0;
 }
